3e0d60637ddc1441b3367470f02236d8caea53e1
suchith720 <suchith720@gmail.com>
2016-09-28 22:33:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get('/article-one', function(req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function(req, res){
-   res.send('Article-two requested and will be served here'); 
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function(req, res){
-   res.send('Article-three requested and will be served here'); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
