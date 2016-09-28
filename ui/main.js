3a52be096aca06ba0929c91cae9b053388ed03c6
suchith720 <suchith720@gmail.com>
2016-09-29 01:09:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,9 +8,9 @@ var img=document.getElementById('dino');
 var marginLeft =0;
 function moveRight()
 {
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+5;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick = function (){
-    var interval= setInterval(moveRight,100);
+    var interval= setInterval(moveRight,50);
 };
