f98755626830188f8ab3ce43e2254a60e9204929
suchith720 <suchith720@gmail.com>
2016-10-01 18:44:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,19 @@
+console.log('Loaded!');
+//CHANGE THE TEXT OF MAIN-TEXT DIV
+var element=document.getElementById('main-text');
+element.innerHTML='SUCHITH PRABHU';
+
+//move image
+var img=document.getElementById('dino');
+var marginLeft =0;
+function moveRight()
+{
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick = function (){
+    var interval= setInterval(moveRight,50);
+};
 
 
 var button = document.getElementById('counter');
@@ -11,7 +27,7 @@ button.onclick = function(){
   // Capture the response and store it in variable
   request.onstatechange= function()
   {
-      if(request.readyState === XmlhttpRequest.DONE)
+      if(request.readyState === XMLHttpRequest.DONE)
       {
           //Take some action
           if(request.status === 200)
