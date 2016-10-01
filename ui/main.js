64d5b05f0815bd6e097c8ebae0ccacc7f51ee8ca
suchith720 <suchith720@gmail.com>
2016-10-01 20:11:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,6 +47,7 @@ button.onclick = function(){
   request.send(null);
 };*/
 var button=document.getElementById('counter');
+var counter = 0;
 button.onclick= function() {
     counter=counter+1;
     var span = document.getElementById('count');
