bceefa615503ba966c694ce9175ec2489d27ea66
suchith720 <suchith720@gmail.com>
2016-10-01 20:20:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,4 @@
-console.log('Loaded!');
-//CHANGE THE TEXT OF MAIN-TEXT DIV
-var element=document.getElementById('main-text');
-element.innerHTML='SUCHITH PRABHU';
 
-//move image
-var img=document.getElementById('dino');
-var marginLeft =0;
-function moveRight()
-{
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function (){
-    var interval= setInterval(moveRight,50);
-};
 
 //XMLHttprequest part - API 
 
