8ae6e7c2f4cc7547b1ec4e3497c8f2e8ddd5c081
suchith720 <suchith720@gmail.com>
2016-10-01 18:06:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,18 @@ function moveRight()
 img.onclick = function (){
     var interval= setInterval(moveRight,50);
 };
+
+
+var button = document.getElementById('counter');
+var counter=0;
+
+button.onclick = function(){
+  //Make a request to the counter endpoint
+  
+  // Capture the response and store it in variable
+  
+  //render the variable in the correct span
+  counter=counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+};
